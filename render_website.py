434cb6daed4cc81a59db3bf1ad54542852d5f7e4
tumkir <tumkir@gmail.com>
2020-10-20 02:49:39 +0300

    refactoring code

--- render_website.py
+++ render_website.py
@@ -6,34 +6,48 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
 from more_itertools import chunked
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
 
-with open('./media/books_data.json', 'r') as my_file:
-    books_data_json = my_file.read()
+def read_json():
+    with open('./media/books_data.json', 'r') as my_file:
+        books_data_json = my_file.read()
+
+    books_data = json.loads(books_data_json)
+    return books_data
 
 
-books_data = json.loads(books_data_json)
+def render_html_page(books_on_page, page_counts, current_page):
+    template = env.get_template('template.html')
 
-number_of_books_on_page = 10
-books_data_chunked = chunked(books_data, number_of_books_on_page)
-page_counts = ceil(len(books_data) / number_of_books_on_page)
+    books_on_page_two_column = chunked(books_on_page, 2)
+    rendered_page = template.render(books_on_page=books_on_page_two_column, page_counts=page_counts, current_page=current_page)
+
+    with open(f'pages/index{current_page}.html', 'w', encoding='utf8') as file:
+        file.write(rendered_page)
 
 
 def on_reload():
-    template = env.get_template('template.html')
     makedirs('pages', exist_ok=True)
+
+    number_of_books_on_page = 10
+
+    books_data = read_json()
+    books_data_chunked = chunked(books_data, number_of_books_on_page)
+    page_counts = ceil(len(books_data) / number_of_books_on_page)
+
     for current_page, books_on_page in enumerate(books_data_chunked, start=1):
-        books_on_page_two_column = chunked(books_on_page, 2)
-        rendered_page = template.render(books_on_page=books_on_page_two_column, page_counts=page_counts, current_page=current_page)
-        with open(f'pages/index{current_page}.html', 'w', encoding='utf8') as file:
-            file.write(rendered_page)
+        render_html_page(books_on_page, page_counts, current_page)
 
 
-on_reload()
+def run_server():
+    server = Server()
+    server.watch('template.html', on_reload)
+    server.serve(root='.')
 
-server = Server()
-server.watch('template.html', on_reload)
-server.serve(root='.')
+
+if __name__ == "__main__":
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+    on_reload()
+    run_server()
